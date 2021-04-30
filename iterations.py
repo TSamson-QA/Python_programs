9cab81ac12312b4a3fe90f8a9190a89dc152219a
Tom-Samson <TSamson@qa.com>
2021-04-30 15:23:02 +0100

    added collections, iterations.py

---
+++ iterations.py
@@ -0,0 +1,11 @@
+count = 0
+while count <5:
+    name = input("Please enter your name:" )
+    print(f"{name} is awesome!")
+    count += 1
+
+
+#for i in range(10,21,2):
+    #print(i)
+
+#prints 10,12,14,16,18,20
