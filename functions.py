4a78e4ca5c4333270e795ae5be4639b3b5ee5c62
Tom-Samson <TSamson@qa.com>
2021-05-04 11:31:24 +0100

    added functions.py

---
+++ functions.py
@@ -0,0 +1,24 @@
+h_grade = 101
+a_grade = 101
+f_grade = 101
+
+def grade_calc(h_score, a_score, f_score):
+    final_grade = h_score + a_score + f_score
+    return final_grade
+
+
+while h_grade > 25:
+    print("Please enter a valid score!")
+    h_grade = int(input("Please input Homework Score out of 25: "))
+    
+while a_grade > 50:
+    print("Please enter a valid score!")
+    a_grade = int(input("Please input Assessment Score out of 50: "))
+    
+while f_grade > 100:
+    print("Please enter a valid score!")
+    f_grade = int(input("Please input Exam Score out of 100: "))
+    
+final = ((grade_calc(h_grade, a_grade, f_grade)) / 175) * 100
+
+print("Final percentage score: ", final , "%")
